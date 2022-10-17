4058dc96acd2cf5b749da26a22d129db06fd8191
bookslomot <alexgol2101@gmail.com>
2022-10-17 15:14:07 +0700

    add CR posts

---
+++ microblog/blog.py
@@ -0,0 +1,22 @@
+from typing import List
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from core.utils import get_db
+from . import service
+from .schemas import PostCreate, PostList
+
+router = APIRouter()
+
+
+@router.get('/', response_model=List[PostList])
+def post_lst(db: Session = Depends(get_db)):
+    return service.get_post_list(db)
+
+
+@router.post('/')
+def post_list(item: PostCreate, db: Session = Depends(get_db)):
+    return service.creat_post(db, item)
+
+
+
+
